b3ca852be7b3905c489f580c55278a3638ac9830
MaxenceGiraud <maxence9giraud@gmail.com>
2021-10-15 11:22:29 +0200

    Remove old commented code for gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -14,19 +14,6 @@ import pandas as pd
 
 import time
 
-import gc
-def get_gc(stop=True):
-    ''' Print objects in the garbage collector '''
-    for obj in gc.get_objects():
-        try:
-            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
-                print(type(obj), obj.size())
-        except:
-            pass
-    
-    if stop : 
-        _ = input("\n Press enter to continue \n")
-
 
 def grid_search(model,data,param_model_grid,learning_grid,file,cols):
     # Get param list to try
@@ -38,7 +25,6 @@ def grid_search(model,data,param_model_grid,learning_grid,file,cols):
     score_table = []
 
     for i_param_model in param_model_list : 
-        
         for i_learning_param in learning_list : 
 
             print(f"{i_param_model}\n{i_learning_param}\n")
@@ -48,40 +34,18 @@ def grid_search(model,data,param_model_grid,learning_grid,file,cols):
             # Create model
             modeli = model(d=data,**i_param_model)
 
-
             # Train 
             modeli,test_metrics = train_temporal(modeli,data,**i_learning_param)
-
-            # Compute MRR
-            # print("train ranks")
-            # train_ranks = get_ranks(modeli,torch.tensor(data.train_data_idxs),torch.tensor(data.train_data_idxs[:,-1]),device=i_learning_param['device'])
-            # print('mrr + hits')
-            # train_mrr = compute_MRR(train_ranks)
-            # train_hits1 = compute_hits(train_ranks,1)
-            # train_hits3 = compute_hits(train_ranks,3)
-            # train_hits10 = compute_hits(train_ranks,10)
-
-            # test_ranks = get_ranks(modeli,torch.tensor(data.test_data_idxs),torch.tensor(data.test_data_idxs[:,-1]),device=i_learning_param['device'])
-            # test_mrr = compute_MRR(test_ranks)
-            # test_hits1 = compute_hits(test_ranks,1)
-            # test_hits3 = compute_hits(test_ranks,3)
-            # test_hits10 = compute_hits(test_ranks,10)
-
+            
+            # Save data to file
             total_time = time.time()-t_start
-
-            # score_table.append([i_param_model,i_learning_param,train_mrr,train_hits1,train_hits3,train_hits10,test_mrr,test_hits1,test_hits3,test_hits10,total_time])
-
             score_table.append([i_param_model,i_learning_param,*test_metrics,total_time])
-
             pd.DataFrame(score_table,columns=cols).to_csv(file)
 
 
             # Clear memory cache
             del modeli
             torch.cuda.empty_cache()
-
-
-    
     
     return score_table
 
@@ -98,17 +62,7 @@ def main():
 
     learning_param_grid = {'learning_rate':[0.003,0.002,0.001,0.0008,0.0005,0.0002,0.0001],'n_iter':[400],'batch_size':[128],'device':[device]} 
 
-
-    # cols = ["Model Parameters","Learning Parameters","Train MRR", "Train Hits@1", "Train Hits@3", "Train Hits@10","Test MRR", "Test Hits@1", "Test Hits@3", "Test Hits@10","Time"]
     cols = ["Model Parameters","Learning Parameters","Test MRR", "Test Hits@1", "Test Hits@3", "Test Hits@10","Time"]
-
-
-    ### TESTS ##############################
-    # parameter_grid_tr = {'de':[300],'dr':[50],'dt':[50],'ranks':[10],'cuda':[cud],"input_dropout":[0.],"hidden_dropout1":[0.],"hidden_dropout2":[0.]}
-    # parameter_grid_cpd = {'de':[20],'dr':[10],'dt':[10],'cuda':[cud],"input_dropout":[0.],"hidden_dropout1":[0.],"hidden_dropout2":[0.]}
-    # learning_param_grid = {'learning_rate':[0.00001],'n_iter':[100],'batch_size':[128],'device':[DEVICE]}
-    ##############################
-
 
     print("-------------------------------------------------------------------\n Training TuckER with CPD on core \n--------------------------------------------------------------------")
     scores_cpd = grid_search(TuckERCPD,data,parameter_grid_cpd,learning_param_grid,file="results/CPD.csv",cols=cols)
