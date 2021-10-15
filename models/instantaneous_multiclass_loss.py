5af15ececa073e6ce2da7b4563eb1856b1fdef4e
MaxenceGiraud <maxence9giraud@gmail.com>
2021-10-15 11:09:10 +0200

    Remove old file

--- models/instantaneous_multiclass_loss.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-def instantaneous_multiclass_loss(input,target):
-    target = torch.tensor(target,dtype=bool)
-    return torch.sigmoid(-input[target].sum() + torch.log(torch.exp(input[~target]).sum()))
